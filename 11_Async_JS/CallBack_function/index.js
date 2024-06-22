b41de0a75ace52535606225cf6af8c89513a3500
aditya dadial <aditya.dadial@network.elancoah.com>
2024-06-22 17:55:57 +0530

    docs: updated async docs

--- 11_Async_JS/CallBack_function/index.js
+++ 11_Async_JS/CallBack_function/index.js
@@ -1,4 +1,6 @@
-//Making HTTP Request
+//Callback function
+let todos =(callback)=>{
+    //Making HTTP Request
 let request= new XMLHttpRequest(); //Use to make request to server
 console.log(request,request.readyState);
 
@@ -20,15 +22,41 @@ console.log(request,request.readyState);
 request.addEventListener('readystatechange',()=>{
     // console.log(request,request.readyState)
     if(request.readyState=== 4 && request.status == 200){ //Status code
-        console.log(request,request.responseText)
+        // console.log(request,request.responseText)
+        //Extacting JSON data
+        let data=JSON.parse(request.responseText);
+        callback(undefined,data);
+    }
+    else if(request.readyState ===4){
+        // console.log("Data could not be fetch");
+        callback("Data cannot be fetch",undefined);
     }
 })
 
 //Set up the request
-request.open("Get","https://jsonplaceholder.typicode.com/todos");
+// request.open("Get","https://jsonplaceholder.typicode.com/todos");
+request.open("Get","data.json");
+
 
 //Sending the request
 request.send()
 
+}
 
 
+console.log(1)
+console.log(2)
+
+//Async code will take some time to execute
+todos((error,Data)=>{
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log(Data);
+    }
+
+})
+
+console.log(3);
+console.log(4);
