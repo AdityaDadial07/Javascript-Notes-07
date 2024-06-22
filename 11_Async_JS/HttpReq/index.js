9b8fb18cf024de6f9db612d20a43ec481c123f42
aditya dadial <aditya.dadial@network.elancoah.com>
2024-06-22 17:31:21 +0530

    docs: updated async js

--- 11_Async_JS/HttpReq/index.js
+++ 11_Async_JS/HttpReq/index.js
@@ -1,3 +1,34 @@
 //Making HTTP Request
 let request= new XMLHttpRequest(); //Use to make request to server
-console.log(request);
+console.log(request,request.readyState);
+
+//Value  state     Description
+//0      unsend    Client have created. open() not call yet.
+//1      opened    open() is called
+//2      Header_Received  send() haved been called and headers
+//3      Loading   responseText hold partial data
+//4      Done      The operation is complete
+
+
+//HTTP response status code
+// Informational response(100-199)
+// Successful response(200-299)
+// Redirectional response(300-399)
+// Client error response(400-499)
+// Server error respones(500-599)
+
+request.addEventListener('readystatechange',()=>{
+    // console.log(request,request.readyState)
+    if(request.readyState===4){
+        console.log(request.responseText)
+    }
+})
+
+//Set up the request
+request.open("Get","https://jsonplaceholder.typicode.com/todos");
+
+//Sending the request
+request.send()
+
+
+
