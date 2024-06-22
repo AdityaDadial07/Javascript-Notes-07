2254cf95fe4b76fe06929c07013d6e73bebd8c74
aditya dadial <aditya.dadial@network.elancoah.com>
2024-06-22 16:45:11 +0530

    docs: updated async doc

---
+++ 11_Async_JS/Async_js/index.js
@@ -0,0 +1,10 @@
+//Asynchronous javascript
+//Example
+console.log(1);
+console.log(2);
+
+//Async code
+setTimeout(()=>{console.log("Call back function after 3 seconds")},3000); //This block will not stop execution of rest of code 
+
+console.log(3);
+console.log(4);
---
+++ 11_Async_JS/HttpReq/index.js
@@ -0,0 +1,3 @@
+//Making HTTP Request
+let request= new XMLHttpRequest(); //Use to make request to server
+console.log(request);
